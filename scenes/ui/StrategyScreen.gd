57d56011d41e72209a438ac851a2fc87a2224a6c
Geopardi00 <mjkettun@gmail.com>
2026-08-23 14:55:09 +0300

    Refine strategy screen panel layout

--- scenes/ui/StrategyScreen.gd
+++ scenes/ui/StrategyScreen.gd
@@ -1,3 +1,4 @@
+@tool
 extends Control
 class_name StrategyScreen
 
@@ -9,6 +10,158 @@ signal debug_force_cpu_win_requested
 
 const GameDataScript := preload("res://scripts/data/GameData.gd")
 const UiAssetsScript := preload("res://scripts/data/UiAssets.gd")
+
+@export_category("Strategy Panel Layout")
+@export_range(0.0, 180.0, 1.0, "suffix:px") var panel_top_position := 76.0:
+	set(value):
+		panel_top_position = value
+		_queue_layout_preview()
+@export_range(0.0, 100.0, 1.0, "suffix:px") var panel_spacing := 20.0:
+	set(value):
+		panel_spacing = value
+		_queue_layout_preview()
+@export_range(340.0, 500.0, 1.0, "suffix:px") var side_panel_width := 430.0:
+	set(value):
+		side_panel_width = value
+		_queue_layout_preview()
+
+@export_category("Status Panel")
+@export_range(-150.0, 150.0, 1.0, "suffix:px") var status_panel_x := 0.0:
+	set(value):
+		status_panel_x = value
+		_queue_layout_preview()
+@export_range(-150.0, 150.0, 1.0, "suffix:px") var status_panel_y := 0.0:
+	set(value):
+		status_panel_y = value
+		_queue_layout_preview()
+@export_range(-40.0, 100.0, 1.0, "suffix:px") var status_text_x := 22.0:
+	set(value):
+		status_text_x = value
+		_queue_layout_preview()
+@export_range(0.0, 180.0, 1.0, "suffix:px") var status_text_y := 82.0:
+	set(value):
+		status_text_y = value
+		_queue_layout_preview()
+
+@export_category("Big Rocket Panel")
+@export_range(-150.0, 150.0, 1.0, "suffix:px") var big_rocket_panel_x := 0.0:
+	set(value):
+		big_rocket_panel_x = value
+		_queue_layout_preview()
+@export_range(-150.0, 150.0, 1.0, "suffix:px") var big_rocket_panel_y := 0.0:
+	set(value):
+		big_rocket_panel_y = value
+		_queue_layout_preview()
+@export_range(-100.0, 100.0, 1.0, "suffix:px") var big_rocket_title_x := 0.0:
+	set(value):
+		big_rocket_title_x = value
+		_queue_layout_preview()
+@export_range(-100.0, 100.0, 1.0, "suffix:px") var big_rocket_title_y := 4.0:
+	set(value):
+		big_rocket_title_y = value
+		_queue_layout_preview()
+@export_range(-100.0, 100.0, 1.0, "suffix:px") var big_rocket_stats_x := 0.0:
+	set(value):
+		big_rocket_stats_x = value
+		_queue_layout_preview()
+@export_range(-100.0, 100.0, 1.0, "suffix:px") var big_rocket_stats_y := 4.0:
+	set(value):
+		big_rocket_stats_y = value
+		_queue_layout_preview()
+@export_range(-100.0, 100.0, 1.0, "suffix:px") var big_rocket_button_x := 0.0:
+	set(value):
+		big_rocket_button_x = value
+		_queue_layout_preview()
+@export_range(-100.0, 100.0, 1.0, "suffix:px") var big_rocket_button_y := 4.0:
+	set(value):
+		big_rocket_button_y = value
+		_queue_layout_preview()
+
+@export_category("Space Shuttle Panel")
+@export_range(-150.0, 150.0, 1.0, "suffix:px") var shuttle_panel_x := 0.0:
+	set(value):
+		shuttle_panel_x = value
+		_queue_layout_preview()
+@export_range(-150.0, 150.0, 1.0, "suffix:px") var shuttle_panel_y := 0.0:
+	set(value):
+		shuttle_panel_y = value
+		_queue_layout_preview()
+@export_range(-100.0, 100.0, 1.0, "suffix:px") var shuttle_title_x := 0.0:
+	set(value):
+		shuttle_title_x = value
+		_queue_layout_preview()
+@export_range(-100.0, 100.0, 1.0, "suffix:px") var shuttle_title_y := 4.0:
+	set(value):
+		shuttle_title_y = value
+		_queue_layout_preview()
+@export_range(-100.0, 100.0, 1.0, "suffix:px") var shuttle_stats_x := 0.0:
+	set(value):
+		shuttle_stats_x = value
+		_queue_layout_preview()
+@export_range(-100.0, 100.0, 1.0, "suffix:px") var shuttle_stats_y := 4.0:
+	set(value):
+		shuttle_stats_y = value
+		_queue_layout_preview()
+@export_range(-100.0, 100.0, 1.0, "suffix:px") var shuttle_button_x := 0.0:
+	set(value):
+		shuttle_button_x = value
+		_queue_layout_preview()
+@export_range(-100.0, 100.0, 1.0, "suffix:px") var shuttle_button_y := 4.0:
+	set(value):
+		shuttle_button_y = value
+		_queue_layout_preview()
+
+@export_category("SpinLaunch Panel")
+@export_range(-150.0, 150.0, 1.0, "suffix:px") var spinlaunch_panel_x := 0.0:
+	set(value):
+		spinlaunch_panel_x = value
+		_queue_layout_preview()
+@export_range(-150.0, 150.0, 1.0, "suffix:px") var spinlaunch_panel_y := 0.0:
+	set(value):
+		spinlaunch_panel_y = value
+		_queue_layout_preview()
+@export_range(-100.0, 100.0, 1.0, "suffix:px") var spinlaunch_title_x := 0.0:
+	set(value):
+		spinlaunch_title_x = value
+		_queue_layout_preview()
+@export_range(-100.0, 100.0, 1.0, "suffix:px") var spinlaunch_title_y := 4.0:
+	set(value):
+		spinlaunch_title_y = value
+		_queue_layout_preview()
+@export_range(-100.0, 100.0, 1.0, "suffix:px") var spinlaunch_stats_x := 0.0:
+	set(value):
+		spinlaunch_stats_x = value
+		_queue_layout_preview()
+@export_range(-100.0, 100.0, 1.0, "suffix:px") var spinlaunch_stats_y := 4.0:
+	set(value):
+		spinlaunch_stats_y = value
+		_queue_layout_preview()
+@export_range(-100.0, 100.0, 1.0, "suffix:px") var spinlaunch_button_x := 0.0:
+	set(value):
+		spinlaunch_button_x = value
+		_queue_layout_preview()
+@export_range(-100.0, 100.0, 1.0, "suffix:px") var spinlaunch_button_y := 4.0:
+	set(value):
+		spinlaunch_button_y = value
+		_queue_layout_preview()
+
+@export_category("News Panel")
+@export_range(-150.0, 150.0, 1.0, "suffix:px") var news_panel_x := 0.0:
+	set(value):
+		news_panel_x = value
+		_queue_layout_preview()
+@export_range(-150.0, 150.0, 1.0, "suffix:px") var news_panel_y := 0.0:
+	set(value):
+		news_panel_y = value
+		_queue_layout_preview()
+@export_range(-40.0, 100.0, 1.0, "suffix:px") var news_text_x := 22.0:
+	set(value):
+		news_text_x = value
+		_queue_layout_preview()
+@export_range(0.0, 180.0, 1.0, "suffix:px") var news_text_y := 82.0:
+	set(value):
+		news_text_y = value
+		_queue_layout_preview()
 
 @onready var day_label: Label = %DayLabel
 @onready var faction_label: Label = %FactionLabel
@@ -25,6 +178,10 @@ var vehicle_cards: Dictionary = {}
 
 
 func _ready() -> void:
+	_apply_layout_settings()
+	if Engine.is_editor_hint():
+		return
+
 	vehicle_cards = {
 		"big_rocket": {
 			"icon": %BigRocketIcon,
@@ -66,6 +223,122 @@ func _ready() -> void:
 		button.pressed.connect(vehicle_selected.emit.bind(vehicle_id))
 
 	UiAssetsScript.apply_text_outline(self)
+
+
+func _queue_layout_preview() -> void:
+	if not is_inside_tree():
+		return
+	_apply_layout_settings()
+
+
+func _apply_layout_settings() -> void:
+	var top_spacer := get_node_or_null("Layout/PanelTopSpacer") as Control
+	var panels := get_node_or_null("Layout/Panels") as HBoxContainer
+	var status_panel := get_node_or_null("Layout/Panels/StatusPanel") as Control
+	var news_panel := get_node_or_null("Layout/Panels/NewsPanel") as Control
+	var status_art := get_node_or_null("Layout/Panels/StatusPanel/PanelArt") as Control
+	var news_art := get_node_or_null("Layout/Panels/NewsPanel/PanelArt") as Control
+	var status_margin := get_node_or_null("Layout/Panels/StatusPanel/Margin") as MarginContainer
+	var news_margin := get_node_or_null("Layout/Panels/NewsPanel/Margin") as MarginContainer
+	if top_spacer != null:
+		top_spacer.custom_minimum_size.y = panel_top_position
+	if panels != null:
+		panels.add_theme_constant_override("separation", int(panel_spacing))
+	if status_panel != null:
+		status_panel.custom_minimum_size.x = side_panel_width
+	if news_panel != null:
+		news_panel.custom_minimum_size.x = side_panel_width
+	_set_stretched_offsets(status_art, Vector4.ZERO, Vector2(status_panel_x, status_panel_y))
+	_set_stretched_offsets(status_margin, Vector4(22.0, 82.0, 34.0, 87.0), Vector2(status_panel_x, status_panel_y))
+	_set_stretched_offsets(news_art, Vector4.ZERO, Vector2(news_panel_x, news_panel_y))
+	_set_stretched_offsets(news_margin, Vector4(22.0, 82.0, 34.0, 87.0), Vector2(news_panel_x, news_panel_y))
+	_set_text_margin_position(status_margin, status_panel_x + status_text_x, status_panel_y + status_text_y)
+	_set_text_margin_position(news_margin, news_panel_x + news_text_x, news_panel_y + news_text_y)
+	_apply_vehicle_panel_layout(
+		"BigRocketCard",
+		Vector2(big_rocket_panel_x, big_rocket_panel_y),
+		Vector2(big_rocket_title_x, big_rocket_title_y),
+		Vector2(big_rocket_stats_x, big_rocket_stats_y),
+		Vector2(big_rocket_button_x, big_rocket_button_y),
+		"BigRocketTitle",
+		"BigRocketStats",
+		"BigRocketButton",
+		Vector2(0.0, 5.0)
+	)
+	_apply_vehicle_panel_layout(
+		"ShuttleCard",
+		Vector2(shuttle_panel_x, shuttle_panel_y),
+		Vector2(shuttle_title_x, shuttle_title_y),
+		Vector2(shuttle_stats_x, shuttle_stats_y),
+		Vector2(shuttle_button_x, shuttle_button_y),
+		"ShuttleTitle",
+		"ShuttleStats",
+		"ShuttleButton",
+		Vector2(0.0, 4.0)
+	)
+	_apply_vehicle_panel_layout(
+		"SpinLaunchCard",
+		Vector2(spinlaunch_panel_x, spinlaunch_panel_y),
+		Vector2(spinlaunch_title_x, spinlaunch_title_y),
+		Vector2(spinlaunch_stats_x, spinlaunch_stats_y),
+		Vector2(spinlaunch_button_x, spinlaunch_button_y),
+		"SpinLaunchTitle",
+		"SpinLaunchStats",
+		"SpinLaunchButton",
+		Vector2(0.0, 5.0)
+	)
+	if panels != null:
+		panels.queue_sort()
+	var layout := get_node_or_null("Layout") as VBoxContainer
+	if layout != null:
+		layout.queue_sort()
+	queue_redraw()
+
+
+func _set_text_margin_position(margin: MarginContainer, x: float, y: float) -> void:
+	if margin == null:
+		return
+	margin.offset_left = x
+	margin.offset_top = y
+	margin.offset_right = x + 12.0
+	margin.offset_bottom = y + 5.0
+
+
+func _apply_vehicle_panel_layout(
+	card_name: String,
+	panel_offset: Vector2,
+	title_offset: Vector2,
+	stats_offset: Vector2,
+	button_offset: Vector2,
+	title_name: String,
+	stats_name: String,
+	button_name: String,
+	title_base_position: Vector2
+) -> void:
+	var base_path := "Layout/Panels/VehiclePanel/%s" % card_name
+	var panel_art := get_node_or_null("%s/PanelArt" % base_path) as Control
+	var margin := get_node_or_null("%s/Margin" % base_path) as Control
+	var content_path := "%s/Margin/Content" % base_path
+	var title := get_node_or_null("%s/%s" % [content_path, title_name]) as Control
+	var stats := get_node_or_null("%s/%s" % [content_path, stats_name]) as Control
+	var button := get_node_or_null("%s/%s" % [content_path, button_name]) as Control
+	_set_stretched_offsets(panel_art, Vector4.ZERO, panel_offset)
+	_set_stretched_offsets(margin, Vector4(28.0, 28.0, -28.0, -28.0), panel_offset)
+	if title != null:
+		title.position = title_base_position + title_offset
+	if stats != null:
+		stats.position = Vector2(12.0, 370.0) + stats_offset
+	if button != null:
+		button.position = Vector2(32.0, 518.0) + button_offset
+
+
+func _set_stretched_offsets(control: Control, base: Vector4, delta: Vector2) -> void:
+	if control == null:
+		return
+	control.offset_left = base.x + delta.x
+	control.offset_top = base.y + delta.y
+	control.offset_right = base.z + delta.x
+	control.offset_bottom = base.w + delta.y
 
 
 func setup(summary: Dictionary, show_debug_actions: bool) -> void:
--- scripts/tests/StrategyScreenSmokeTest.gd
+++ scripts/tests/StrategyScreenSmokeTest.gd
@@ -29,6 +29,47 @@ func _run() -> void:
 	if strategy_screen.day_label.text != "Day 0":
 		_fail("Strategy screen did not populate the day label.")
 		return
+	if not strategy_screen.has_node("Layout/Panels/NewsPanel"):
+		_fail("Strategy screen is missing the right-side news panel.")
+		return
+	if strategy_screen.news_label.get_parent().get_parent().get_parent().get_parent().name != "NewsPanel":
+		_fail("Strategy screen news feed is not inside the right-side news panel.")
+		return
+	var status_panel := strategy_screen.get_node("Layout/Panels/StatusPanel") as Control
+	var vehicle_panel := strategy_screen.get_node("Layout/Panels/VehiclePanel") as Control
+	var news_panel := strategy_screen.get_node("Layout/Panels/NewsPanel") as Control
+	if not status_panel.position.x < vehicle_panel.position.x or not vehicle_panel.position.x < news_panel.position.x:
+		_fail("Strategy screen panels are not ordered status, vehicles, then news.")
+		return
+	var target_viewport_width := float(ProjectSettings.get_setting("display/window/size/viewport_width", 1920))
+	if news_panel.position.x + news_panel.size.x > target_viewport_width:
+		_fail("Strategy screen right-side news panel extends beyond the target viewport.")
+		return
+	strategy_screen.panel_spacing = 24.0
+	strategy_screen.status_text_x = 26.0
+	strategy_screen.news_text_y = 90.0
+	strategy_screen.big_rocket_panel_x = 7.0
+	strategy_screen.big_rocket_stats_y = 9.0
+	await process_frame
+	var panels := strategy_screen.get_node("Layout/Panels") as HBoxContainer
+	var status_margin := strategy_screen.get_node("Layout/Panels/StatusPanel/Margin") as MarginContainer
+	var news_margin := strategy_screen.get_node("Layout/Panels/NewsPanel/Margin") as MarginContainer
+	var big_rocket_art := strategy_screen.get_node("Layout/Panels/VehiclePanel/BigRocketCard/PanelArt") as Control
+	var big_rocket_title := strategy_screen.get_node("Layout/Panels/VehiclePanel/BigRocketCard/Margin/Content/BigRocketTitle") as Control
+	var big_rocket_stats := strategy_screen.get_node("Layout/Panels/VehiclePanel/BigRocketCard/Margin/Content/BigRocketStats") as Control
+	if panels.get_theme_constant("separation") != 24:
+		_fail("Strategy screen panel spacing slider did not update the layout.")
+		return
+	if status_margin.offset_left != strategy_screen.status_panel_x + 26.0 or news_margin.offset_top != strategy_screen.news_panel_y + 90.0:
+		_fail("Strategy screen text position sliders did not update the layout.")
+		return
+	if (
+		big_rocket_art.offset_left != strategy_screen.big_rocket_panel_x
+		or big_rocket_title.position.y != 5.0 + strategy_screen.big_rocket_title_y
+		or big_rocket_stats.position.y != 379.0
+	):
+		_fail("Strategy screen vehicle panel and text sliders did not update independently.")
+		return
 
 	print("Strategy screen smoke test passed.")
 	quit(0)
